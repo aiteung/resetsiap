81c67336153c1f9101263c04def96d0fdea70864
Valen Rionald <vrionald@gmail.com>
2024-02-15 16:45:28 +0700

    Fix(ResetPassword): Deactivate reset password mhs

--- handler.go
+++ handler.go
@@ -8,32 +8,33 @@ import (
 )
 
 func Handler(Pesan model.IteungMessage, db *sql.DB) (reply string) {
-	mahasiswa, _ := GetMahasiswaByPhoneNumber(db, Pesan.Phone_number)
-	if strings.Contains(Pesan.Message, "mahasiswa") {
+	//mahasiswa, _ := GetMahasiswaByPhoneNumber(db, Pesan.Phone_number)
+	//if strings.Contains(Pesan.Message, "mahasiswa") {
+	//	// Split pesan menjadi kata-kata
+	//	pesanSplit := strings.Fields(Pesan.Message)
+	//	foundSiap := false
+	//	for i, kata := range pesanSplit {
+	//		if kata == "siap" && i+1 < len(pesanSplit) {
+	//			// Password baru adalah kata setelah "siap"
+	//			PasswordBaru := pesanSplit[i+2]
+	//			reply = ResetPassword(db, PasswordBaru, Pesan)
+	//			foundSiap = true
+	//			break
+	//		}
+	//	}
+	//	// Jika tidak ada kata "siap" atau password baru, berikan pesan error
+	//	if !foundSiap {
+	//		reply = "Keyword kakak belum benar nihh, kakak harus ganti password dengan cara 'Iteung ganti password siap mahasiswa [password_baru]'. Maaciww kakakkk"
+	//	}
+	//}
+	if strings.Contains(Pesan.Message, "dosen") {
 		// Split pesan menjadi kata-kata
 		pesanSplit := strings.Fields(Pesan.Message)
 		foundSiap := false
 		for i, kata := range pesanSplit {
 			if kata == "siap" && i+1 < len(pesanSplit) {
 				// Password baru adalah kata setelah "siap"
-				PasswordBaru := pesanSplit[i+2]
-				reply = ResetPassword(db, PasswordBaru, Pesan)
-				foundSiap = true
-				break
-			}
-		}
-		// Jika tidak ada kata "siap" atau password baru, berikan pesan error
-		if !foundSiap {
-			reply = "Keyword kakak belum benar nihh, kakak harus ganti password dengan cara 'Iteung ganti password siap mahasiswa [password_baru]'. Maaciww kakakkk"
-		}
-	} else if strings.Contains(Pesan.Message, "dosen") {
-		// Split pesan menjadi kata-kata
-		pesanSplit := strings.Fields(Pesan.Message)
-		foundSiap := false
-		for i, kata := range pesanSplit {
-			if kata == "siap" && i+1 < len(pesanSplit) {
-				// Password baru adalah kata setelah "siap"
-				PasswordBaru := pesanSplit[i+2]
+				PasswordBaru := strings.ToLower(pesanSplit[i+2])
 				reply = ResetPasswordDosen(db, Pesan, PasswordBaru)
 				foundSiap = true
 				break
@@ -43,23 +44,25 @@ func Handler(Pesan model.IteungMessage, db *sql.DB) (reply string) {
 		if !foundSiap {
 			reply = "Keyword kakak belum benar nihh, kakak harus ganti password dengan cara 'Iteung ganti password siap dosen [password_baru]'. Maaciww kakakkk"
 		}
-	} else if strings.Contains(Pesan.Message, "cara") {
-		reply = CaraResetPassword(mahasiswa)
-	} else if strings.Contains(Pesan.Message, "approval") {
-		if !IsPhoneNumberExist(db, Pesan) {
-			reply = MessageUpdateNomorDiSiap()
-		} else {
-			// Periksa persetujuan dosen wali
-			tahun := GetCurrentAcademicYear()
-			_, prw, _ := CheckMahasiswaApproval(db, Pesan, tahun)
-			if prw.AppDosenWali == 2 {
-				reply = MessageSudahApproval(mahasiswa, prw)
-			} else if prw.AppDosenWali == 0 {
-				reply = MessageBelumApproval(mahasiswa, prw)
-			}
-		}
 	} else {
 		return "Terjadi Error"
 	}
+	//else if strings.Contains(Pesan.Message, "cara") {
+	//	reply = CaraResetPassword(mahasiswa)
+	//}
+	//else if strings.Contains(Pesan.Message, "approval") {
+	//	if !IsPhoneNumberExist(db, Pesan) {
+	//		reply = MessageUpdateNomorDiSiap()
+	//	} else {
+	//		// Periksa persetujuan dosen wali
+	//		tahun := GetCurrentAcademicYear()
+	//		_, prw, _ := CheckMahasiswaApproval(db, Pesan, tahun)
+	//		if prw.AppDosenWali == 2 {
+	//			reply = MessageSudahApproval(mahasiswa, prw)
+	//		} else if prw.AppDosenWali == 0 {
+	//			reply = MessageBelumApproval(mahasiswa, prw)
+	//		}
+	//	}
+	//}
 	return
 }
