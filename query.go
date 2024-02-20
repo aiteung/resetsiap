619a5b56f32625fe5898f4f79de26c8a0a77cbed
Valen Rionald <vrionald@gmail.com>
2024-02-20 15:35:43 +0700

    Ref(Reset): Clear code di file query dan service

--- query.go
+++ query.go
@@ -1,13 +1,9 @@
 package siappkg
 
 import (
-	"context"
 	"crypto/md5"
 	"database/sql"
 	"encoding/hex"
-	"fmt"
-	"log"
-
 	"github.com/aiteung/module/model"
 )
 
@@ -59,66 +55,6 @@ func GetDosenByPhoneNumber(db *sql.DB, phoneNumber string) (TblDosen, error) {
 	}
 
 	return result, nil
-}
-
-func CheckMahasiswaApproval(db *sql.DB, Pesan model.IteungMessage, TahunAkademik string) (TblMhs, Perwalian, error) {
-	// Mendapatkan tahun akademik saat ini dalam format "YYYY-YYYY"
-	TahunAkademik = GetCurrentAcademicYear()
-
-	// Query SQL untuk menggabungkan data dari tabel tblMHS dan Perwalian
-	query := `
-        SELECT m.Nim, m.Nama_Mhs, m.Tlp_Mhs, p.AppDosenWali
-        FROM tblMHS m
-        LEFT JOIN Perwalian p ON m.Nim = p.Nim
-        WHERE m.Tlp_Mhs = ? AND p.Thn_Akademik = ?
-    `
-
-	var mhs TblMhs
-	var prw Perwalian
-	row := db.QueryRowContext(context.Background(), query, Pesan.Phone_number, TahunAkademik)
-	if err := row.Scan(&mhs.Nim, &mhs.NamaMhs, &mhs.TlpMhs, &prw.AppDosenWali); err != nil {
-		return TblMhs{}, Perwalian{}, err
-	}
-
-	prw.TahunAkademik = TahunAkademik
-
-	return mhs, prw, nil
-}
-
-func IsPhoneNumberExist(db *sql.DB, Pesan model.IteungMessage) bool {
-	// Query database untuk memeriksa apakah nomor handphone sudah ada
-	query := "SELECT COUNT(*) FROM tblMHS WHERE Tlp_Mhs = ?"
-	var count int
-	err := db.QueryRow(query, Pesan.Phone_number).Scan(&count)
-	if err != nil {
-		log.Println(err)
-		return false // Terjadi kesalahan saat mengakses database
-	}
-	return count > 0
-}
-
-func GetTahunAkademik(db *sql.DB) (*AcademicYear, error) {
-	// Query to get the active academic year
-	query := "SELECT DISTINCT Thn_Akademik FROM Perwalian WHERE Tgl_Prw <= GETDATE() AND GETDATE() <= DATEADD(YEAR, 1, Tgl_Prw)"
-	rows, err := db.Query(query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	// Loop through the query results
-	for rows.Next() {
-		var tahunAkademik string
-		if err := rows.Scan(&tahunAkademik); err != nil {
-			return nil, err
-		}
-
-		// Return the AcademicYear struct with the retrieved value
-		return &AcademicYear{ThnAkademik: tahunAkademik}, nil
-	}
-
-	// If no rows were returned, you may want to handle this case accordingly
-	return nil, fmt.Errorf("no active academic year found")
 }
 
 func ResetPasswordDosen(db *sql.DB, Pesan model.IteungMessage, newPassword string) (reply string) {
