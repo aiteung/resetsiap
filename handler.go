b39ca29564f4ae8ba48ad0c1bd812eb22b51a2e8
Valen Rionald <vrionald@gmail.com>
2024-02-20 11:59:15 +0700

    Fix(Reset): Perbaikan handling function reset pass mhs dan dosen

--- handler.go
+++ handler.go
@@ -9,40 +9,44 @@ import (
 
 func Handler(Pesan model.IteungMessage, db *sql.DB) (reply string) {
 	mahasiswa, _ := GetMahasiswaByPhoneNumber(db, Pesan.Phone_number)
-	if strings.Contains(Pesan.Message, "mahasiswa") {
+	if (strings.Contains(Pesan.Message, "ganti") || strings.Contains(Pesan.Message, "reset")) && strings.Contains(Pesan.Message, "mahasiswa") {
 		// Split pesan menjadi kata-kata
 		pesanSplit := strings.Fields(Pesan.Message)
 		foundSiap := false
-		for i, kata := range pesanSplit {
-			if kata == "siap" && i+1 < len(pesanSplit) {
-				// Password baru adalah kata setelah "siap"
-				PasswordBaru := pesanSplit[i+2]
-				reply = ResetPassword(db, PasswordBaru, Pesan)
+		var PasswordBaru string
+		for i := 0; i < len(pesanSplit); i++ {
+			kata := pesanSplit[i]
+			if kata == "siap" && i+2 < len(pesanSplit) {
+				// Pastikan bahwa indeks i+2 tidak melebihi panjang pesanSplit
+				PasswordBaru = pesanSplit[i+2]
 				foundSiap = true
 				break
 			}
 		}
 		// Jika tidak ada kata "siap" atau password baru, berikan pesan error
 		if !foundSiap {
-			reply = "Keyword kakak belum benar nihh, kakak harus ganti password dengan cara 'Iteung ganti password siap mahasiswa [password_baru]'. Maaciww kakakkk"
+			PasswordBaru = "sariasih54"
 		}
-	} else if strings.Contains(Pesan.Message, "dosen") {
+		reply = ResetPassword(db, PasswordBaru, Pesan)
+	} else if (strings.Contains(Pesan.Message, "ganti") || strings.Contains(Pesan.Message, "reset")) && strings.Contains(Pesan.Message, "dosen") {
 		// Split pesan menjadi kata-kata
 		pesanSplit := strings.Fields(Pesan.Message)
 		foundSiap := false
-		for i, kata := range pesanSplit {
-			if kata == "siap" && i+1 < len(pesanSplit) {
-				// Password baru adalah kata setelah "siap"
-				PasswordBaru := strings.ToLower(pesanSplit[i+2])
-				reply = ResetPasswordDosen(db, Pesan, PasswordBaru)
+		var PasswordBaru string
+		for i := 0; i < len(pesanSplit); i++ {
+			kata := pesanSplit[i]
+			if kata == "siap" && i+2 < len(pesanSplit) {
+				// Pastikan bahwa indeks i+2 tidak melebihi panjang pesanSplit
+				PasswordBaru = pesanSplit[i+2]
 				foundSiap = true
 				break
 			}
 		}
 		// Jika tidak ada kata "siap" atau password baru, berikan pesan error
 		if !foundSiap {
-			reply = "Keyword kakak belum benar nihh, kakak harus ganti password dengan cara 'Iteung ganti password siap dosen [password_baru]'. Maaciww kakakkk"
+			PasswordBaru = "sariasih54"
 		}
+		reply = ResetPasswordDosen(db, Pesan, PasswordBaru)
 	} else if strings.Contains(Pesan.Message, "cara") {
 		reply = CaraResetPassword(mahasiswa)
 	} else if strings.Contains(Pesan.Message, "approval") {
